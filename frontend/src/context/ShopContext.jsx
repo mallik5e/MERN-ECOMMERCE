b23b68c6e8fffa9836b5e0df8ceb9ec2abc0101f
MALLIKARJUNASWAMY H M <94712213+mallik5e@users.noreply.github.com>
2024-11-20 20:46:22 +0530

    Update ShopContext.jsx

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -8,7 +8,7 @@ export const ShopContext = createContext();
 const ShopContextProvider = (props) => {
     const currency = '$';
     const delivery_fee = 10;
-    const backendUrl = 'https://mern-ecommerce-x1gy.onrender.com/';
+    const backendUrl = 'https://mern-ecommerce-x1gy.onrender.com';
     const [search,setSearch] = useState('');
     const [showSearch,setShowSearch] = useState(false);
     const [cartItems,setCartItems] = useState({});
