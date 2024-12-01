0e765051df1e60ec9a2f4c4726d199358e081241
MALLIKARJUNASWAMY H M <94712213+mallik5e@users.noreply.github.com>
2024-12-01 19:37:29 +0530

    Update Product.jsx

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -66,15 +66,11 @@ const Product = () => {
                        ))}
                 </div>
               </div>
-              {isLoggedIn ? 
+            
               <button onClick={()=>addToCart(productData._id,size)} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>
               ADD TO CART
               </button>
-              : 
-              <NavLink to='/login' className='bg-black text-white px-80 py-30 text-sm active:bg-gray-700'>
-              Login First
-              </NavLink>
-              }
+             
               <hr className='mt-8 sm:w-4/5' />
               <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
                     <p>100% original product.</p>
